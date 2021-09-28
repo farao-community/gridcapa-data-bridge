378f07c4b44fa5cb9372e5fb067be03762108c0e
Montigny <alexandre.montigny@rte-france.com>
2021-09-28 17:35:02 +0200

    Add SFTP source to gridcapa-data-bridge

---
+++ src/main/java/com/farao_community/farao/gridcapa/data_bridge/SftpSource.java
@@ -0,0 +1,74 @@
+package com.farao_community.farao.gridcapa.data_bridge;
+
+import com.jcraft.jsch.ChannelSftp.LsEntry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.file.remote.session.CachingSessionFactory;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.integration.sftp.inbound.SftpInboundFileSynchronizer;
+import org.springframework.integration.sftp.inbound.SftpInboundFileSynchronizingMessageSource;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * @author Alexandre Montigny {@literal <alexandre.montigny at rte-france.com>}
+ */
+@Configuration
+public class SftpSource {
+    public static final String SYNCHRONIZE_TEMP_DIRECTORY_PREFIX = "gridcapa-data-bridge";
+
+    @Value("${data-bridge.sources.sftp.host}")
+    private String host;
+    @Value("${data-bridge.sources.sftp.port}")
+    private int port;
+    @Value("${data-bridge.sources.sftp.username}")
+    private String username;
+    @Value("${data-bridge.sources.sftp.password}")
+    private String password;
+    @Value("${data-bridge.sources.sftp.base-directory}")
+    private String baseDirectory;
+
+    @Bean
+    public SessionFactory<LsEntry> sftpSessionFactory() {
+        DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory(true);
+        factory.setHost(host);
+        factory.setPort(port);
+        factory.setUser(username);
+        factory.setPassword(password);
+        factory.setAllowUnknownKeys(true);
+        return new CachingSessionFactory<LsEntry>(factory);
+    }
+
+    @Bean
+    public SftpInboundFileSynchronizer sftpInboundFileSynchronizer() {
+        SftpInboundFileSynchronizer synchronizer = new SftpInboundFileSynchronizer(sftpSessionFactory());
+        synchronizer.setDeleteRemoteFiles(false);
+        synchronizer.setRemoteDirectory(baseDirectory);
+        return synchronizer;
+    }
+
+    @Bean
+    @InboundChannelAdapter(channel = "sftpSourceChannel", poller = @Poller(fixedDelay = "${data-bridge.sources.sftp.polling-delay-in-ms}"))
+    public MessageSource<File> sftpMessageSource() throws IOException {
+        SftpInboundFileSynchronizingMessageSource source = new SftpInboundFileSynchronizingMessageSource(sftpInboundFileSynchronizer());
+        source.setLocalDirectory(Files.createTempDirectory(SYNCHRONIZE_TEMP_DIRECTORY_PREFIX).toFile());
+        source.setAutoCreateLocalDirectory(true);
+        return source;
+    }
+
+    @Bean
+    public IntegrationFlow sftpPreprocessFlow() {
+        return IntegrationFlows.from("sftpSourceChannel")
+                .channel("archivesChannel")
+                .get();
+    }
+}
