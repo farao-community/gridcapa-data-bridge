3891c02af75acd3ca807dfafad95eb842d75c98c
thieulinseb <sebastien.thieulin@rte-france.com>
2022-08-23 09:09:39 +0200

    tests units corrections

--- src/test/java/com/farao_community/farao/gridcapa/data_bridge/ConfigurationTest.java
+++ src/test/java/com/farao_community/farao/gridcapa/data_bridge/ConfigurationTest.java
@@ -16,14 +16,14 @@ class ConfigurationTest {
     @Test
     void checkReadConfiguration() {
 
-        assertEquals(1, remoteFileConfiguration.getDataBridgeList().size());
+        assertEquals(1, remoteFileConfiguration.getBridges().size());
         assertEquals("Europe/Paris", remoteFileConfiguration.getZoneId());
-        assertEquals("Europe/Paris", remoteFileConfiguration.getDataBridgeList().get(0).getZoneId());
-        assertEquals("CSE_D2CC", remoteFileConfiguration.getDataBridgeList().get(0).getTargetProcess());
-        assertEquals("CGM", remoteFileConfiguration.getDataBridgeList().get(0).getFileType());
-        assertEquals("regex_test", remoteFileConfiguration.getDataBridgeList().get(0).getFileRegex());
-        assertEquals("hourly", remoteFileConfiguration.getDataBridgeList().get(0).getTimeValidity());
-        assertEquals("/data/gridcapa/cse/d2cc/cgms", remoteFileConfiguration.getDataBridgeList().get(0).getFtpDirectory());
-        assertEquals("/cgms", remoteFileConfiguration.getDataBridgeList().get(0).getMinioDirectory());
+        assertEquals("Europe/Paris", remoteFileConfiguration.getBridges().get(0).getZoneId());
+        assertEquals("CSE_D2CC", remoteFileConfiguration.getBridges().get(0).getTargetProcess());
+        assertEquals("CGM", remoteFileConfiguration.getBridges().get(0).getFileType());
+        assertEquals("regex_test", remoteFileConfiguration.getBridges().get(0).getFileRegex());
+        assertEquals("hourly", remoteFileConfiguration.getBridges().get(0).getTimeValidity());
+        assertEquals("/data/gridcapa/cse/d2cc/cgms", remoteFileConfiguration.getBridges().get(0).getFtpDirectory());
+        assertEquals("/cgms", remoteFileConfiguration.getBridges().get(0).getMinioDirectory());
     }
 }
