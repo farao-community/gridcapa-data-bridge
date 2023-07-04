4c10a3a47e16275f103ee8a50585dd46ca3d95de
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2023-07-04 15:25:19 +0200

    add ftp health check to databridge app for k8s

---
+++ src/main/java/com/farao_community/farao/gridcapa/data_bridge/health/FtpHealthIndicator.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)marc
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.gridcapa.data_bridge.health;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.integration.ftp.session.DefaultFtpSessionFactory;
+import org.springframework.integration.ftp.session.FtpSession;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author Marc Schwitzguebel {@literal <marc.schwitzguebel at rte-france.com>}
+ */
+@Component
+public class FtpHealthIndicator implements HealthIndicator {
+
+    @Value("${data-bridge.sources.ftp.host}")
+    private String ftpHost;
+    @Value("${data-bridge.sources.ftp.port}")
+    private int ftpPort;
+    @Value("${data-bridge.sources.ftp.username}")
+    private String ftpUsername;
+    @Value("${data-bridge.sources.ftp.password}")
+    private String ftpPassword;
+    @Value("${data-bridge.sources.ftp.base-directory}")
+    private String ftpBaseDirectory;
+    @Value("${data-bridge.sources.ftp.data-timeout:60000}")
+    private int ftpDataTimeout;
+
+    @Override
+    public Health health() {
+        DefaultFtpSessionFactory ftpSessionFactory = new DefaultFtpSessionFactory();
+        ftpSessionFactory.setHost(ftpHost);
+        ftpSessionFactory.setPort(ftpPort);
+        ftpSessionFactory.setUsername(ftpUsername);
+        ftpSessionFactory.setPassword(ftpPassword);
+        ftpSessionFactory.setClientMode(FTPClient.PASSIVE_LOCAL_DATA_CONNECTION_MODE);
+        ftpSessionFactory.setDataTimeout(ftpDataTimeout);
+
+        FtpSession session = ftpSessionFactory.getSession();
+
+        try {
+            if (session.test() && session.exists(ftpBaseDirectory)) {
+                return Health.up().build();
+            }
+        } catch (IOException e) {
+            return Health.down().build();
+        }
+        return Health.down().build();
+    }
+}
