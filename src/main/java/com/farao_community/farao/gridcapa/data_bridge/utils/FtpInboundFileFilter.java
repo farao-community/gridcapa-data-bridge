14326e80332a29122117caaea7b462163a1c88eb
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2024-05-17 17:04:55 +0200

    adding forgotten copyright

--- src/main/java/com/farao_community/farao/gridcapa/data_bridge/utils/FtpInboundFileFilter.java
+++ src/main/java/com/farao_community/farao/gridcapa/data_bridge/utils/FtpInboundFileFilter.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.gridcapa.data_bridge.utils;
 
 import com.farao_community.farao.gridcapa.data_bridge.configuration.FileMetadataConfiguration;
@@ -10,6 +16,9 @@ import org.springframework.integration.metadata.PropertiesPersistingMetadataStor
 
 import java.nio.file.Path;
 
+/**
+ * @author Marc Schwitzguebel {@literal <marc.schwitzguebel  at rte-france.com>}
+ */
 public final class FtpInboundFileFilter {
 
     private FtpInboundFileFilter() {
