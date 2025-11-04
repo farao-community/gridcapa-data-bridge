000c95c21fb2de2675c0d988a42017edeafacf42
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2025-11-04 11:35:27 +0100

    increase test coverage

--- src/test/java/com/farao_community/farao/gridcapa/data_bridge/FileMetadataProviderTest.java
+++ src/test/java/com/farao_community/farao/gridcapa/data_bridge/FileMetadataProviderTest.java
@@ -341,6 +341,23 @@ class FileMetadataProviderTest {
         assertAllInputFileMetadataEquals(metadataMap, "CORE-VALID-IDCC", "VERTICES", "", "");
     }
 
+    @Test
+    void checkBadTimeValidityThrows() {
+        mockConfig(
+                "CORE-VALID-IDCC",
+                "VERTICES",
+                "DECINAL",
+                "(?<year>[0-9]{4})(?<month>[0-9]{2})(?<day>[0-3]{1}[0-9]{1})-(?<hour>[0-2]{1}[0-9]{1})(?<minute>00[abAB]{0,1})-FID2-701-INIT_VIRG_REFBAL_PRES_REPREVERTICES-v(?<version>[0-9]*).(csv|CSV)",
+                "Europe/Paris"
+        );
+        Message<?> ucteFileMessage = MessageBuilder
+                .withPayload("")
+                .setHeader(MinioAdapterConstants.DEFAULT_GRIDCAPA_FILE_NAME_METADATA_KEY, "20250330-0400-FID2-701-INIT_VIRG_REFBAL_PRES_REPREVERTICES-v1.csv")
+                .build();
+        Map<String, String> metadataMap = new HashMap<>();
+        assertThrows(DataBridgeException.class, () -> fileMetadataProvider.populateMetadata(ucteFileMessage, metadataMap));
+    }
+
     void assertAllInputFileMetadataEquals(Map<String, String> actualMetadata, String targetProcess, String fileType, String fileName, String fileValidityInterval) {
         assertEquals(MinioAdapterConstants.DEFAULT_GRIDCAPA_INPUT_GROUP_METADATA_VALUE, actualMetadata.get(FileMetadataProvider.GRIDCAPA_FILE_GROUP_METADATA_KEY));
         assertEquals(targetProcess, actualMetadata.get(FileMetadataProvider.GRIDCAPA_FILE_TARGET_PROCESS_METADATA_KEY));
